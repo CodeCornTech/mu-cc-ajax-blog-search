cc6d376756c3ae3b5412855d7352d25c95a24949
Federico Girolami <federicogirolami.fg@gmail.com>
2026-07-20 22:38:20 +0200

    docs: add scoped integration patches

---
+++ scoped_patches/patches_horseno.php
@@ -0,0 +1,18 @@
+horseno-core/addons/widgets/faq.php
+horseno-core/addons/widgets/contact-box.php
+
+
+
+
+./// hadcoded! Translations
+horseno-core/addons/widgets/blog-category.php.bak
+// ANCHE MALE //
+News Categoria
+Categoria List
+
+
+wordpress/wp-content/themes/horseno/inc/horseno-framework/horseno-options/horseno-options.bugged
+
+
+wordpress/wp-content/plugins/redux-framework/redux-core/inc/classes/class-redux-api.php ##### patched! bloccare aggiornamenti
+wordpress/wp-content/plugins/horseno-core/inc/horsenocore-functions.php ##### patched! bloccare aggiornamenti
